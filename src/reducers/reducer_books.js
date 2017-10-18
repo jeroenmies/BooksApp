9187783fb2bcbdd9adae8a242b2580b96a6a20b6
Jeroen Mies <jeroen.mies@gmail.com>
2017-10-18 12:37:50 +0200

    Completed tutorial version

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: "Ivanhoe", pages: 101},
+        {title: "Javascript: the Good Parts", pages: 39},
+        {title: "Harry Potter", pages: 85},
+        {title: "Robin Hood", pages: 1}
+    ]
+}
